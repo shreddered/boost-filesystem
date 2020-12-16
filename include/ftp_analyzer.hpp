2ac38ed13a88701a992927bad7f0dffc075932d3
Dmitry Kalinin <x.shreddered.x@gmail.com>
2020-12-16 21:09:09 +0300

    changed interface a bit

--- include/ftp_analyzer.hpp
+++ include/ftp_analyzer.hpp
@@ -10,16 +10,21 @@
 #include <ostream>
 #include <regex>
 #include <string>
+// boost headers
+#include <boost/filesystem.hpp>
 
 class FtpAnalyzer final {
 public:
-    explicit FtpAnalyzer(const std::string& root, const std::string& filter);
-    std::map<std::string, BrokerStats> analyze(std::ostream& os);
+    explicit FtpAnalyzer(const std::string& root);
+    std::multimap<std::string, BrokerStats> analyze(std::ostream& os);
 private:
     std::string root_;
     std::regex filter_;
-    void analyzeImpl(const fs::path& currPath, std::ostream& os,
-            std::map<std::string, BrokerStats>& stats);
+    std::multimap<std::string, BrokerStats> stats;
+    void analyzeImpl(const boost::filesystem::path& currPath,
+            std::ostream& os);
+    void insert(const std::string& broker, const std::string& id,
+            const std::string& date);
 }; // class FtpAnalyzer
 
 #endif // INCLUDE_FTP_ANALYZER_HPP_
