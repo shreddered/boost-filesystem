b9e9ccd3bc334535f8d219f245ffe10bf4415c6f
Dmitry Kalinin <x.shreddered.x@gmail.com>
2020-12-16 21:47:50 +0300

    added comparison for BrokerStats (bc of tests)

--- sources/broker_stats.cpp
+++ sources/broker_stats.cpp
@@ -5,3 +5,9 @@
 BrokerStats::BrokerStats(std::size_t total_, const std::string& account_,
         const std::string& lastDate_)
     : lastDate(lastDate_), account(account_), total(total_) {}
+
+bool operator==(const BrokerStats& lhs, const BrokerStats& rhs) {
+    return lhs.total == rhs.total
+        && lhs.account == rhs.account
+        && lhs.lastDate == rhs.lastDate;
+}
